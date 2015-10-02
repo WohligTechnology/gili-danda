d861b94126f867e2310db3e1ecd21c55bca676a6
Chintan Shah <chintan@wohlig.com>
2015-10-02 16:44:11 +0530

    changes for the files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,9 +54,10 @@ module.exports = function (grunt) {
             dist: {
                 src: [
                     './bower_components/jquery/dist/jquery.js',
-                    './js/jquery.flexslider.js',
+                    './bower_components/flexslider/jquery.flexslider-min.js',
                     './bower_components/angular/angular.js',
-                    './js/angular-flexslider.js',
+                    './bower_components/angular-flexslider/angular-flexslider.js',
+                    './bower_components/fancyBox/source/jquery.fancybox.js',
                     './bower_components/bootstrap/dist/js/bootstrap.min.js',
                     './bower_components/angular-sanitize/angular-sanitize.min.js',
                     './bower_components/ui-router/release/angular-ui-router.min.js',
@@ -330,3 +331,4 @@ module.exports = function (grunt) {
     grunt.registerTask('production', ['copy', 'htmlmin', 'less:production', 'ngtemplates', 'cssmin', 'concat', 'uglify', 'compress:css', 'compress:js', 'compress:indexhtml', 'compress:zip']);
     grunt.registerTask('appengine', ['copy:appengine', 'htmlmin:appengine', 'less:production', 'ngtemplates',"cssmin", 'concat', 'uglify', 'copy:jsappengine','copy:cssappengine']);
 };
+

1d236ed806cf0a64b8f8dc60bb2f86c8658a7e09
Dhaval Gala <dhaval.gala59@gmail.com>
2015-10-03 12:26:12 +0530

    changes done which was given

---
+++ img/playbook/School-and-Participant-Registration-Forms/files/search/text_position.js
@@ -0,0 +1,3 @@
+﻿var positionForPages = [{page:1,positions:[]},{page:2,positions:[]},{page:3,positions:[]},{page:4,positions:[]},{page:5,positions:[]},{page:6,positions:[]},{page:7,positions:[]},{page:8,positions:[]},{page:9,positions:[]},{page:10,positions:[]},{page:11,positions:[]},{page:12,positions:[
+]}
+];

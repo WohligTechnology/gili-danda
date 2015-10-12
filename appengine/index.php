056392a0b9e65a61c823fab0bde09ab6c10e1e95
mahesh-wohlig <mahesh@wohlig.com>
2015-10-12 11:29:44 +0530

    changes for zopim chat

--- appengine/index.php
+++ appengine/index.php
@@ -59,6 +59,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   ga('send', 'pageview');
 
 </script>
+
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3Nwhrdv7gDoKNpauyfJgkwTrI8mvEjcl";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+
+
 </body>
 
 </html>

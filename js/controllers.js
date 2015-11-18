8b4a86f0eff19fbdc735219ad8529d023f00d58d
TusharSachde <tushar@wohlig.com>
2015-11-18 19:33:31 +0530

    Done

--- js/controllers.js
+++ js/controllers.js
@@ -339,7 +339,8 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 			if (data == 1) {
 				$scope.school = {};
 				ngDialog.open({
-					template: './views/content/thankyou.html'
+					template: './views/content/thankyou.html',
+					scope: $scope
 				});
 			}
 		}

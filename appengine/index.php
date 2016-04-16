601d83c12ee3c420be1f1e7b904616bce01f92ab
mahesh maurya <mahesh@wohlig.com>
2016-04-16 17:02:55 +0530

    ddd

--- appengine/index.php
+++ appengine/index.php
@@ -36,34 +36,14 @@
   <div class="repeated-item" ui-view>
   </div>
   
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K98G4P"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K98G4P');</script>
 
-  <noscript>
-    <iframe src="//www.googletagmanager.com/ns.html?id=GTM-K98G4P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-  </noscript>
-
-  <script>
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start':
-
-          new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
-
-        '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-
-    })(window, document, 'script', 'dataLayer', 'GTM-K98G4P');
-  </script>
-
-  
   <link rel="canonical" href="http://www.sfanow.in/home">
   <script>
     (function(d, s, id) {

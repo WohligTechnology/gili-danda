cdb408fbd4005e3b96550a3d75cd1cf43993699f
mahesh maurya <mahesh@wohlig.com>
2016-04-26 17:54:20 +0530

    ss

--- appengine/index.php
+++ appengine/index.php
@@ -1,105 +1,113 @@
 <!DOCTYPE html>
 <html lang="en" ng-app="firstapp">
 
-<head ng-controller="headerctrl">
-  <base href="http://sfanow.in/">
-  
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <head ng-controller="headerctrl">
+        <base href="http://sfanow.in/">
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
 
-  <meta name="description" content="{{template.description}}" ng-if="template.description!='';">
-  <meta name="keywords" content="{{template.keywords}}" ng-if="template.keywords!='';">
-  <meta name="description" content="Sports For All (SFA) is an inter-school sporting platform for school children to play, compete and grow in various sports." ng-if="template.keywords=='';">
-  <meta name="keywords" content="Inter school sports, Sports tournaments, Inter school sports in Mumbai, Sports for kids" ng-if="template.description=='';">
+        <meta name="description" content="{{template.description}}" ng-if="template.description!='';">
+        <meta name="keywords" content="{{template.keywords}}" ng-if="template.keywords!='';">
+        <meta name="description" content="Sports For All (SFA) is an inter-school sporting platform for school children to play, compete and grow in various sports." ng-if="template.keywords=='';">
+        <meta name="keywords" content="Inter school sports, Sports tournaments, Inter school sports in Mumbai, Sports for kids" ng-if="template.description=='';">
 
-  <meta property="og:site_name" content="Sports For All (SFA)">
-  <meta property="og:title" content="Sports For All (SFA)">
-  <meta property="og:description" content="Sports For All (SFA) is an inter-school sporting platform for school children to play, compete and grow in various sports.">
-  <meta property="og:type" content="website">
+        <meta property="og:site_name" content="Sports For All (SFA)">
+        <meta property="og:title" content="Sports For All (SFA)">
+        <meta property="og:description" content="Sports For All (SFA) is an inter-school sporting platform for school children to play, compete and grow in various sports.">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="http://sfanow.in/img/fb-banner.jpg">
 
-  <title ng-bind="'SFA - '+template.title">SFA</title>
+        <title ng-bind="'SFA - '+template.title">SFA</title>
 
-  <link rel="shortcut icon" href="img/favicon.ico">
-  <link rel="stylesheet" type="text/css" href="w/w.min.css">
-  <script>
-    var isproduction = true;
-  </script>
-  <script src="w/w.min.js"></script>
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="w/w.min.css">
+        <script>
+            var isproduction = true;
+        </script>
+        <script src="w/w.min.js"></script>
 
+        <!--[if IE]><script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script> <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script><![endif]-->
 
-  <!--[if IE]><script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script><![endif]-->
+    </head>
 
-</head>
+    <body>
+        <div class="repeated-item" ui-view></div>
+        
+        <noscript>
+            <iframe src="//www.googletagmanager.com/ns.html?id=GTM-K98G4P" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <script>
+            (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != 'dataLayer'
+                        ? '&l=' + l
+                        : '';
+                j.async = true;
+                j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-K98G4P');
+        </script>
+        
+        <link rel="canonical" href="http://www.sfanow.in/home">
+        <script>
+            (function(d, s, id) {
+                var js,
+                    fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id))
+                    return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=1589474464632854";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                },
+                i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-<body>
-  <div class="repeated-item" ui-view>
-  </div>
-  
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K98G4P"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K98G4P');</script>
+            ga('create', 'UA-68579769-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        
+        <script type="text/javascript">
+            window.$zopim || (function(d, s) {
+                var z = $zopim = function(c) {
+                        z._.push(c)
+                    },
+                    $ = z.s = d.createElement(s),
+                    e = d.getElementsByTagName(s)[0];
+                z.set = function(o) {
+                    z.set._.push(o)
+                };
+                z._ = [];
+                z.set._ = [];
+                $.async = !0;
+                $.setAttribute("charset", "utf-8");
+                $.src = "//v2.zopim.com/?3Nwhrdv7gDoKNpauyfJgkwTrI8mvEjcl";
+                z.t = +new Date;
+                $.type = "text/javascript";
+                e.parentNode.insertBefore($, e)
+            })(document, "script");
+        </script>
+        
 
-  <link rel="canonical" href="http://www.sfanow.in/home">
-  <script>
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=1589474464632854";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  </script>
-  <script>
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        
+        <script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion_async.js" charset="utf-8"></script>
 
-    ga('create', 'UA-68579769-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-  
-  <script type="text/javascript">
-    window.$zopim || (function(d, s) {
-      var z = $zopim = function(c) {
-          z._.push(c)
-        },
-        $ = z.s =
-        d.createElement(s),
-        e = d.getElementsByTagName(s)[0];
-      z.set = function(o) {
-        z.set.
-        _.push(o)
-      };
-      z._ = [];
-      z.set._ = [];
-      $.async = !0;
-      $.setAttribute("charset", "utf-8");
-      $.src = "//v2.zopim.com/?3Nwhrdv7gDoKNpauyfJgkwTrI8mvEjcl";
-      z.t = +new Date;
-      $.
-      type = "text/javascript";
-      e.parentNode.insertBefore($, e)
-    })(document, "script");
-  </script>
-  
-
-  
-  <script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion_async.js" charset="utf-8"></script>
-
-</body>
+    </body>
 
 </html>
